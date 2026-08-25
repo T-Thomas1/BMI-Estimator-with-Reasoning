abdab286457b3fea35678aa31637784546198d95
T-Thomas1 <tvthomas1524@gmail.com>
2026-08-24 23:30:27 -0400

    q hat implementation 8.24.26

--- get_started/calibrate_bmi.py
+++ get_started/calibrate_bmi.py
@@ -19,8 +19,6 @@ import argparse
 from model import SEDensenet121, SEDensenet201, load_pretrained_densenet #, load_pretrained_densenet201
 from dataset import BMIDataset, df_test, df_train
 from predict_bmi import load_model, predict_bmi
-from mapie.regression import MapieRegressor
-from mapie.conformity_scores import AbsoluteConformityScore
 
 parser = argparse.ArgumentParser(description='BMI Prediction using DenseNet')
 parser.add_argument('--model_path', type=str, default='weights/best_model.ckpt',
@@ -33,25 +31,25 @@ parser.add_argument('--batch_size', type=int, default=32,
 parser.add_argument('--device', type=str, default='auto',
                     choices=['auto', 'cuda', 'cpu'],
                     help='Device to use for computation')
+args = parser.parse_args()
 # Set device
 if args.device == 'auto':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 else: 
     device = args.device
-args = parser.parse_args()
 #Load our dataset
 test_dataset = BMIDataset(df_test)
-test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
-
 calibration_set = Subset(test_dataset, range(0, 350)) #Calibration set
 evaluation_set = Subset(test_dataset, range (350, len(test_dataset))) #Evaluation set
 
+calibration_dataloader = DataLoader(calibration_set, batch_size=args.batch_size, shuffle=False)
+
 def calibrate_bmi(model, dataloader, device="cuda"):
     y_true = []
-    y_pred = []
+    y_hat = []
     model.eval()
     with torch.no_grad():
-        for images, bmis, ids in test_dataloader:
+        for images, bmis, ids in calibration_dataloader:
             images = images.to(device, dtype=torch.float32)
             preds = model(images).cpu().numpy().flatten()
             for p, y in zip(preds, bmis):
@@ -64,6 +62,7 @@ def calibrate_bmi(model, dataloader, device="cuda"):
     alpha = 0.1
     k = int(np.ceil((n + 1) * (1 - alpha)))
     q_hat = float(np.sort(scores)[k-1])
-    
+    print(q_hat)
+
 model = load_model(args.model_path, args.model_type, device) #Load our model
-calibrate_bmi(model, test_dataloder, device)
+calibrate_bmi(model, calibration_dataloader, device)
