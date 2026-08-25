2c9052b7f858036a5defd0462e87a7fa41737fc9
T-Thomas1 <tvthomas1524@gmail.com>
2026-08-24 21:54:34 -0400

    Unsure implementation of regression

--- get_started/calibrate_bmi.py
+++ get_started/calibrate_bmi.py
@@ -1,4 +1,4 @@
-#Conformal Prediction for predict_bmi.py to provide a prediction with interval
+#Conformal Prediction for predict_bmi.py to provide a split conformal regression value. 
 
 import cv2
 import torch
@@ -10,17 +10,39 @@ import os
 from datasets import load_dataset
 import sys
 import torch.nn as nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 import argparse
 from model import SEDensenet121, SEDensenet201, load_pretrained_densenet #, load_pretrained_densenet201
 from dataset import BMIDataset, df_test, df_train
 from predict_bmi import load_model, predict_bmi
+from mapie.regression import MapieRegressor
+from mapie.conformity_scores import AbsoluteConformityScore
 
 #Load our dataset
-sample_dataset = BMIDataset(df_test)
-sample_dataloader = DataLoader(sample_dataset, batch_size=args.batch_size, shuffle=False)
-true_dataset = BMIDataset(df_train)
-true_dataloader = DataLoader(true_dataset, batch_size=args.batch_size, shuffle=False)
+test_dataset = BMIDataset(df_test)
+test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
+train_dataset = BMIDataset(df_train)
+
+train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=False)
+parser = argparse.ArgumentParser(description='BMI Prediction using DenseNet')
+parser.add_argument('--model_path', type=str, default='weights/best_model.ckpt',
+                    help='Path to model checkpoint')
+parser.add_argument('--model_type', type=str, default='densenet121',
+                    choices=['densenet121', 'densenet201'],
+                    help='Type of model to use')
+parser.add_argument('--batch_size', type=int, default=1,
+                    help='Batch size for inference')
+parser.add_argument('--device', type=str, default='auto',
+                    choices=['auto', 'cuda', 'cpu'],
+                    help='Device to use for computation')
+
+args = parser.parse_args()
+
+# Set device
+if args.device == 'auto':
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+else: 
+    device = args.device
 
 #Load model 
 print("Loading model...")
@@ -29,9 +51,15 @@ if model is None:
     print("Failed to load model. Exiting...")
     sys.exit(1)
 
-x_calib = sample_dataset[3000:] #Calibrations
-y_calib = true_dataset[350:]
-calib_dataloader = DataLoader(y_calib, batch_size=args.batch_size, shuffle=False)
+x_train = Subset(test_dataset, range(0, 3000)) #Sample training
+y_train = Subset(test_dataset, range(3000, len(test_dataset))) # Label training
+
+x_calib = Subset(train_dataset, range(0, 350)) #Sample calibration
+y_calib = Subset(train_dataset, range(350, len(train_dataset))) # Label calibration
+
+test_dataloader = DataLoader(y_calib, batch_size=args.batch_size, shuffle=False) #Dataloader for calibration
+
+
 x_eval = sample_dataset[:3000] #Evaluations
 y_eval = true_dataset[:350]
 
@@ -41,31 +69,37 @@ if args.device == 'auto':
 else: 
     device = args.device
 
+
+def construct_prediction_bands(mean_prediction, quantiles):
+    """
+    A function that returns the lower and upper prediction bands for every quantile in quantiles.
+    """
+    
+    prediction_bands = np.zeros((len(mean_prediction), 2, len(quantiles)))
+
+    for i, q in enumerate(quantiles):
+        prediction_bands[:, :, i] = np.stack([
+            mean_prediction - q,
+            mean_prediction + q
+        ], axis=1)
+
+    return prediction_bands
+
+mean_predictor = LinearRegression()
+mean_predictor.fit(X_train, y_train)
+y_train_predict = mean_predictor.predict(X_train)
+
+mapie_regressor = MapieRegressor(mean_predictor, conformity_score=None, cv = 'prefit', method ='base')
+mapie_regressor.fit(x_calib, y_calib)
+y_pred, y_pis = mapie_regressor.predict(x_train, 0.1)
+
 predict_bmi(calib_dataloader, true_dataloader, device) #Calibrate
 
-def calibrate(self, calibrated_dataloader, alpha = 0.1):
+# For the mean predictor, we choose a linear regression model as this matches the ground-truth function
+mean_predictor = LinearRegression()
+mean_predictor.fit(x_calib, y_calib)
+y_calib_predict = mean_predictor.predict(x_calib)
 
-    if self._model is None:
-        raise ValueError("Model is not defined. Please provide a valid model.")
-    
-    self._model.eval()
-    logits_list = [] #Logits to keep track of strengh
-    labels_list = [] #For what the model labels
-    with torch.no_grad():
-        for examples in calibrated_dataloader:
-            tmp_x, tmp_labels = examples[0].to(self._device), examples[1].to(self._device)
-            tmp_logits = self._logits_transformation(self._model(tmp_x)).detach()
-            logits_list.append(tmp_logits)
-            labels_list.append(tmp_labels)
-        logits = torch.cat(logits_list).float()
-        labels = torch.cat(labels_list)
-    self.calculate_threshold(logits, labels, alpha)
+cal_mean_prediction = mean_predictor.predict(x_calib)
 
-def calculate_threshold(self, logits, labels, alpha = 0.1):
-    logits = logits.to(self._device)
-    labels = labels.to(self._device)
-    self.cal_scores = self.score_function(logits, labels)
-    self.q_hat = self._calculate_conformal_value(self.cal_scores, alpha)
-
-def _calculate_conformal_value(self, scores, alpha):
-    return calculate_conformal_value(scores, alpha)
+scores = np.abs(y_calib - cal_mean_prediction) #Split conformal prediction
